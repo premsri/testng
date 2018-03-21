dc26dbe5dcdd3be1fea23599078f0c55fe377cab
premsri <premsri2504@gmail.com>
2018-03-21 17:15:11 +0530

    dfghj

--- orgtest/Tesgt.java
+++ orgtest/Tesgt.java
@@ -3,5 +3,6 @@ package orgtest;
 public class Tesgt {
 public static void main(String[] args) {
 	System.out.println("yesy");
+	System.out.println("ddffff");
 }
 }
---
+++ orgtrest2.java
@@ -0,0 +1,6 @@
+
+public class orgtrest2 {
+public static void main(String[] args) {
+	System.out.println("dfghn");
+}
+}
