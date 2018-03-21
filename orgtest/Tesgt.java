454c70d11efc8ef9ac086ab133054e2749a45c2b
premsri <premsri2504@gmail.com>
2018-03-21 17:11:35 +0530

    sd

---
+++ orgtest/Tesgt.java
@@ -0,0 +1,7 @@
+package orgtest;
+
+public class Tesgt {
+public static void main(String[] args) {
+	System.out.println("yesy");
+}
+}
--- src/test/java/org/mv/seleni/MavenTestNg/TestNG.java
+++
@@ -1,6 +0,0 @@
-package org.mv.seleni.MavenTestNg;
-
-public class TestNG {
-	
-
-}
